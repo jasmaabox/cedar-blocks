a133c361c4e1ccdcaff264c3e278988eb2506496
jasmaa <jasonmaa3955@gmail.com>
2023-09-29 23:26:27 -0400

    Add code generator

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,11 @@ Blockly.defineBlocksWithJsonArray([
     "args0": [
       {
         "type": "field_input",
-        "name": "VALUE"
+        "name": "ARG1"
       },
       {
         "type": "input_value",
-        "name": "VALUE"
+        "name": "ARG2"
       }
     ],
     "inputsInline": true,
@@ -48,24 +48,24 @@ Blockly.defineBlocksWithJsonArray([
     "args0": [
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ARG1",
       },
       {
         "type": "field_dropdown",
-        "name": "VALUE",
+        "name": "OPERATOR",
         "options": [
-          ["==", "equals"],
-          ["!=", "notequals"],
-          [">", "greaterthan"],
-          ["<", "lessthan"],
-          [">=", "greaterthanorequals"],
-          ["<=", "lessthanorequals"],
+          ["==", "=="],
+          ["!=", "!="],
+          [">", ">"],
+          ["<", "<"],
+          [">=", ">="],
+          ["<=", "<="],
           ["in", "in"],
         ],
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ARG2",
       },
     ],
     "output": true,
@@ -74,84 +74,84 @@ Blockly.defineBlocksWithJsonArray([
   },
   {
     "type": "cedar_permit",
-    "message0": 'permit %1 to %2 on %3',
+    "message0": 'permit %1 to do %2 on %3',
     "args0": [
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "PRINCIPAL",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ACTION",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "RESOURCE",
       },
     ],
     "colour": PERMIT_COLOR,
   },
   {
     "type": "cedar_permit_when",
-    "message0": 'permit %1 to %2 on %3 when %4',
+    "message0": 'permit %1 to do %2 on %3 when %4',
     "args0": [
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "PRINCIPAL",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ACTION",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "RESOURCE",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "CONDITION",
       },
     ],
     "colour": PERMIT_COLOR,
   },
   {
     "type": "cedar_forbid",
-    "message0": 'forbid %1 to %2 on %3',
+    "message0": 'forbid %1 to do %2 on %3',
     "args0": [
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "PRINCIPAL",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ACTION",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "RESOURCE",
       },
     ],
     "colour": FORBID_COLOR,
   },
   {
     "type": "cedar_forbid_when",
-    "message0": 'forbid %1 to %2 on %3 when %4',
+    "message0": 'forbid %1 to do %2 on %3 when %4',
     "args0": [
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "PRINCIPAL",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "ACTION",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "RESOURCE",
       },
       {
         "type": "input_value",
-        "name": "VALUE",
+        "name": "CONDITION",
       },
     ],
     "colour": FORBID_COLOR,
@@ -225,3 +225,96 @@ const toolbox = {
 };
 
 const workspace = Blockly.inject('app', { toolbox: toolbox });
+
+
+const cedarGenerator = new Blockly.Generator("cedar");
+
+cedarGenerator.forBlock["cedar_builtin"] = function (block, generator) {
+  const value = block.getFieldValue("VALUE");
+  return [`${value}`, 0];
+}
+
+cedarGenerator.forBlock["cedar_permit"] = function (block, generator) {
+  const principal = generator.valueToCode(block, "PRINCIPAL", 0);
+  const action = generator.valueToCode(block, "ACTION", 0);
+  const resource = generator.valueToCode(block, "RESOURCE", 0);
+  return 'permit(\n' +
+    [principal, action, resource]
+      .map(v => generator.prefixLines(v, generator.INDENT)).join(',\n') +
+    '\n);';
+}
+
+cedarGenerator.forBlock["cedar_permit_when"] = function (block, generator) {
+  const principal = generator.valueToCode(block, "PRINCIPAL", 0);
+  const action = generator.valueToCode(block, "ACTION", 0);
+  const resource = generator.valueToCode(block, "RESOURCE", 0);
+  const condition = generator.valueToCode(block, "CONDITION", 0);
+  return 'permit(\n' +
+    [principal, action, resource]
+      .map(v => generator.prefixLines(v, generator.INDENT)).join(',\n') +
+    '\n)\nwhen {\n' +
+    generator.prefixLines(condition, generator.INDENT) +
+    '\n};';
+}
+
+cedarGenerator.forBlock["cedar_forbid"] = function (block, generator) {
+  const principal = generator.valueToCode(block, "PRINCIPAL", 0);
+  const action = generator.valueToCode(block, "ACTION", 0);
+  const resource = generator.valueToCode(block, "RESOURCE", 0);
+  return 'forbid(\n' +
+    [principal, action, resource]
+      .map(v => generator.prefixLines(v, generator.INDENT)).join(',\n') +
+    '\n);';
+}
+
+cedarGenerator.forBlock["cedar_forbid_when"] = function (block, generator) {
+  const principal = generator.valueToCode(block, "PRINCIPAL", 0);
+  const action = generator.valueToCode(block, "ACTION", 0);
+  const resource = generator.valueToCode(block, "RESOURCE", 0);
+  const condition = generator.valueToCode(block, "CONDITION", 0);
+  return 'forbid(\n' +
+    [principal, action, resource]
+      .map(v => generator.prefixLines(v, generator.INDENT)).join(',\n') +
+    '\n)\nwhen {\n' +
+    generator.prefixLines(condition, generator.INDENT) +
+    '\n};';
+}
+
+cedarGenerator.forBlock["text"] = function (block, generator) {
+  const value = block.getFieldValue("TEXT");
+  return [`"${value}"`, 0];
+}
+
+cedarGenerator.forBlock["cedar_scope"] = function (block, generator) {
+  const arg1 = block.getFieldValue("ARG1");
+  const arg2 = generator.valueToCode(block, "ARG2", 0);
+  return [`${arg1}::${arg2}`, 0];
+}
+
+cedarGenerator.forBlock["cedar_binary_operator"] = function (block, generator) {
+  const arg1 = generator.valueToCode(block, "ARG1", 0);
+  const arg2 = generator.valueToCode(block, "ARG2", 0);
+  const operator = block.getFieldValue("OPERATOR");
+  return [`${arg1} ${operator} ${arg2}`, 0];
+}
+
+cedarGenerator.forBlock["lists_create_with"] = function (block, generator) {
+  const values = [];
+  // @ts-ignore
+  for (let i = 0; i < block.itemCount_; i++) {
+    const valueCode = generator.valueToCode(block, `ADD${i}`, 0);
+    if (valueCode) {
+      values.push(valueCode);
+    }
+  }
+  const valueString = values.join(',\n');
+  const code = valueString.length === 0
+    ? '[]'
+    : '[\n' + generator.prefixLines(valueString, generator.INDENT) + '\n]';
+  return [code, 0];
+}
+
+workspace.addChangeListener(() => {
+  const code = cedarGenerator.workspaceToCode(workspace);
+  console.log(code);
+});
